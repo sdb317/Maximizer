9d63d3383bfca9015498a5297351a9f42d0c90ea
sbell <simon.bell@epfl.ch>
2017-07-21 17:14:43 +0200

    Fix buffer overflow for very long window names

--- Maximizer.cpp
+++ Maximizer.cpp
@@ -3,15 +3,20 @@
 
 #include "stdafx.h"
 
+#define MIN_ALL 419
+
 using namespace std;
 
-TCHAR Title[256];
+TCHAR Title[4096];
+
+long TaskbarSize = 0;
 
 struct Monitor
 {
 	HMONITOR hMonitor;
 	RECT MonitorRect;
 	vector<HWND> WindowList;
+	bool SubtractTaskbar;
 };
 
 vector<Monitor*> MonitorList;
@@ -25,12 +30,17 @@ BOOL CALLBACK MonitorEnumProc(
 {
 	Monitor* pMonitor = new Monitor;
 	pMonitor->hMonitor = hMonitor;
+	pMonitor->SubtractTaskbar = false;
 	//CopyRect(&(pMonitor->MonitorRect), lprcMonitor);
 	MONITORINFO MonitorInfo;
 	ZeroMemory(&MonitorInfo, sizeof(MonitorInfo));
 	MonitorInfo.cbSize = sizeof(MONITORINFO);
 	GetMonitorInfo(pMonitor->hMonitor,&MonitorInfo);
-	CopyRect(&(pMonitor->MonitorRect), &(MonitorInfo.rcWork));
+	CopyRect(&(pMonitor->MonitorRect), &(MonitorInfo.rcWork)); // .rcWork from GetMonitorInfo doesn't factor in TaskBar size sometimes!
+	if (MonitorInfo.rcWork.bottom < MonitorInfo.rcMonitor.bottom)
+		TaskbarSize = (MonitorInfo.rcMonitor.bottom - MonitorInfo.rcWork.bottom);
+	else
+		pMonitor->SubtractTaskbar = true;
 	MonitorList.push_back(pMonitor);
 	return TRUE;
 }
@@ -42,20 +52,22 @@ BOOL CALLBACK EnumWindowsProc(
 {
 	ZeroMemory(Title, sizeof(Title));
 	GetWindowText(hwnd, Title, sizeof(Title) - 1);
-	if (_tcslen(Title)>0) // Ignore windows with no titles
-		if (!(_tcsstr(Title, L"Maximizer"))) // Ignore this program
-			if (!(_tcscmp(Title, L"Microsoft Edge") == 0) && !(_tcscmp(Title, L"Program Manager") == 0)) // Ignore Windows hidden desktop bits
-				if (GetWindow(hwnd, GW_CHILD) != NULL)
-					if (IsWindowVisible(hwnd))
-						if (IsIconic(hwnd) == 0) // Only tile windows that aren't already minimised
-						{
 #ifdef _DEBUG
-							wcout << Title << endl;
+	wcout << _T("Found: ") << Title << endl;
 #endif
-							HMONITOR hMonitor=MonitorFromWindow(hwnd,NULL); // Need to add it to the correct monitor's window list
+	if (IsIconic(hwnd) == 0) // Only tile windows that aren't already minimised
+		if (IsWindowVisible(hwnd))
+			if (_tcslen(Title) > 0) // Ignore windows with no titles
+				if (!(_tcsstr(Title, L"Maximizer.exe"))) // Ignore this program
+					if (!(_tcscmp(Title, L"Microsoft Edge") == 0) && !(_tcscmp(Title, L"Program Manager") == 0)) // Ignore Windows hidden desktop bits
+						if ((GetWindow(hwnd, GW_CHILD) != NULL) || (_tcscmp(Title, L"Microsoft Visual Studio") != 0)) {
+#ifdef _DEBUG
+							wcout << _T("Found: ") << Title << endl;
+#endif
+							HMONITOR hMonitor = MonitorFromWindow(hwnd, NULL); // Need to add it to the correct monitor's window list
 							std::for_each(
-								MonitorList.begin(), 
-								MonitorList.end(), 
+								MonitorList.begin(),
+								MonitorList.end(),
 								[hMonitor, hwnd](Monitor* pMonitor) { if (pMonitor->hMonitor == hMonitor) pMonitor->WindowList.push_back(hwnd); }
 							);
 						}
@@ -67,21 +79,51 @@ int _tmain(
 	_TCHAR* argv[]
 	)
 {
-	EnumDisplayMonitors(NULL,NULL,(MONITORENUMPROC)MonitorEnumProc,NULL); // Build a list of monitors...
+	/*
+	DISPLAY_DEVICE DisplayDevice;
+	ZeroMemory(&DisplayDevice, sizeof(DisplayDevice));
+	DisplayDevice.cb = sizeof(DisplayDevice);
+	DWORD iDevNum = 0;
+	while (EnumDisplayDevices(NULL, iDevNum, &DisplayDevice, EDD_GET_DEVICE_INTERFACE_NAME)) {
+		//LONG ChangeDisplaySettingsEx( // Use this to fix wonky screen positions
+		//DisplayDevice.DeviceName,
+		//_In_ DEVMODE *lpDevMode,
+		//HWND    hwnd,
+		//_In_ DWORD   dwflags,
+		//_In_ LPVOID  lParam
+		//);
+		iDevNum++;
+	}
+	return 0;
+	*/
+	EnumDisplayMonitors(NULL, NULL, (MONITORENUMPROC)MonitorEnumProc, NULL); // Build a list of monitors...
 	LPARAM lParam = 0;
-	EnumDesktopWindows(NULL,(WNDENUMPROC)EnumWindowsProc,lParam); // ...and add each window to one of them
+	EnumDesktopWindows(NULL, (WNDENUMPROC)EnumWindowsProc, lParam); // ...and add each window to one of them
 	for (std::vector<Monitor*>::iterator i = MonitorList.begin(); i != MonitorList.end(); ++i){
 		if ((*i)->WindowList.size()) {
 			if ((((*i)->MonitorRect.right) - ((*i)->MonitorRect.left)) < (((*i)->MonitorRect.bottom) - ((*i)->MonitorRect.top))) { // Portrait
 				int Width = ((*i)->MonitorRect.right) - ((*i)->MonitorRect.left);
-				int Height = (int)floor((((*i)->MonitorRect.bottom) - ((*i)->MonitorRect.top)) / (*i)->WindowList.size());
+				int Height = (int)floor((((*i)->MonitorRect.bottom) - ((*i)->MonitorRect.top) - ((*i)->SubtractTaskbar ? TaskbarSize : 0)) / (*i)->WindowList.size());
 				int j = 0;
 				for (const auto &hwnd : (*i)->WindowList) {
+					int Left = ((*i)->MonitorRect.left);
+					int Top = ((*i)->MonitorRect.top) + (Height * j);
+#ifdef _DEBUG
+					ZeroMemory(Title, sizeof(Title));
+					GetWindowText(hwnd, Title, sizeof(Title) - 1);
+					wcout << _T("Tiling: ") << Title << endl << Left << _T(",") << Top << _T(",") << Width << _T(",") << Height << endl;
+#endif
+					if (_tcscmp(Title, L"Microsoft Visual Studio") != 0) {
+						Left += 1;
+						Top += 1;
+						Width += -2;
+						Height += -2;
+					}
 					SetWindowPos(
 						hwnd, 
 						HWND_TOP, 
-						((*i)->MonitorRect.left),
-						((*i)->MonitorRect.top) + (Height * j),
+						Left,
+						Top,
 						Width, 
 						Height,
 						SWP_SHOWWINDOW
@@ -91,14 +133,21 @@ int _tmain(
 			}
 			else { // Landscape
 				int Width = (int)floor((((*i)->MonitorRect.right) - ((*i)->MonitorRect.left)) / (*i)->WindowList.size());
-				int Height = ((*i)->MonitorRect.bottom) - ((*i)->MonitorRect.top);
+				int Height = ((*i)->MonitorRect.bottom) - ((*i)->MonitorRect.top) - ((*i)->SubtractTaskbar ? TaskbarSize : 0);
 				int j = 0;
 				for (const auto &hwnd : (*i)->WindowList) {
+					int Left = ((*i)->MonitorRect.left);
+					int Top = ((*i)->MonitorRect.top) + (Height * j);
+#ifdef _DEBUG
+					ZeroMemory(Title, sizeof(Title));
+					GetWindowText(hwnd, Title, sizeof(Title) - 1);
+					wcout << _T("Tiling: ") << Title << endl << Left << _T(",") << Top << _T(",") << Width << _T(",") << Height << endl;
+#endif
 					SetWindowPos(
 						hwnd,
 						HWND_TOP,
-						((*i)->MonitorRect.left) + (Width * j),
-						((*i)->MonitorRect.top),
+						Left,
+						Top,
 						Width,
 						Height,
 						SWP_SHOWWINDOW
@@ -110,6 +159,8 @@ int _tmain(
 		delete *i;
 	}
 	MonitorList.clear();
+	HWND lTrayHwnd = FindWindow(_T("Shell_TrayWnd"), NULL);
+	SendMessage(lTrayHwnd, WM_COMMAND, MIN_ALL, 0); // Minimize all windows
 	return 0;
 }
 
